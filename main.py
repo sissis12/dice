e4f39b885fccb566de67e3d1b36b73f0c2132cf8
sissis12 <115008914+sissis12@users.noreply.github.com>
2022-12-20 13:53:53 +0900

    main.blocks, main.ts, main.py 업데이트하기

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+def on_gesture_shake():
+    basic.show_number(randint(1, 6))
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
 def on_forever():
-    pass
+    basic.show_icon(IconNames.HEART)
+    basic.pause(100)
+    basic.show_icon(IconNames.YES)
+    basic.pause(100)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(randint(1, 6))
+})
 input.onGesture(Gesture.Shake, function () {
     basic.showNumber(randint(1, 6))
 })
